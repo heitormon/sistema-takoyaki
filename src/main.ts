52b6b577c31c7b5411f377603b18f7fad079ab5d
Heitor Nobuo Matsusaki Nozu <heitor.nozu@ebanx.com>
2023-08-17 19:01:40 -0300

    add layout

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
+import hbs = require('hbs');
+
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(
@@ -11,7 +13,7 @@ async function bootstrap() {
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
-
+  hbs.registerPartials(join(__dirname, '..', 'views', 'partials'));
   await app.listen(3000);
 }
 bootstrap();
